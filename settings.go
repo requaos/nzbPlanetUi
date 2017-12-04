24d77cbc1076b00ab0578b03b95616a339eb72ab
Raymond Skinner <reqpro@requaos.com>
2017-12-04 02:43:40 -0500

    removed storm and now directly use boltDB

--- settings.go
+++ settings.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
 
-	"github.com/asdine/storm"
 	"github.com/boltdb/bolt"
 	"github.com/spf13/viper"
 )
@@ -23,36 +21,73 @@ func getSettings() map[string]string {
 		fmt.Printf("Config file not found...%s", err.Error())
 	}
 	settingsFile := viper.GetStringMapString("settings")
-	db, err := storm.Open("my.db", storm.BoltOptions(0600, &bolt.Options{Timeout: 1 * time.Second}))
-	if err != nil {
-		fmt.Printf("Error opening DB...%s", err.Error())
-	}
-	defer db.Close()
+	fmt.Println(len(settingsFile))
 	if len(settingsFile) > 0 {
-		db.Set("settings", "set", settingsFile)
+		err = settingsToDB(settingsFile)
+		if err != nil {
+			fmt.Printf("DB Update error...%s", err.Error())
+		}
 	}
-	var settings map[string]string
-	db.Get("settings", "set", settings)
-	for i, x := range settings {
-		fmt.Printf("Key: % 9s Value: %s", i, x)
-	}
+	settings := settingsFromDB()
+	fmt.Println("Returning from getSettings()")
 	return settings
 }
 
 func setSettings(nzbSite string, nzbKey string, sabSite string, sabKey string) {
-	settingsFile := map[string]string{
+	settings := map[string]string{
 		"nzbsite": nzbSite,
 		"nzbkey":  nzbKey,
 		"sabsite": sabSite,
 		"sabkey":  sabKey,
 	}
-	db, err := storm.Open("my.db", storm.BoltOptions(0600, &bolt.Options{Timeout: 1 * time.Second}))
+	settingsToDB(settings)
+	startingUp = true
+	Settings = settings
+	SABnzbd = SABnzbdSession()
+}
+
+func settingsFromDB() map[string]string {
+	m := make(map[string]string)
+	db, err := bolt.Open("settings.db", 0644, nil)
 	if err != nil {
-		fmt.Printf("Error opening DB...%s", err.Error())
+		fmt.Printf("Error opening DB...%s\n", err.Error())
 	}
 	defer db.Close()
-	if len(settingsFile) > 0 {
-		db.Set("settings", "set", settingsFile)
+	db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte("settings"))
+		for _, x := range []string{"nzbsite", "nzbkey", "sabsite", "sabkey"} {
+			v := b.Get([]byte(x))
+			m[x] = string(v)
+			fmt.Println("Retrieving -> " + x + ": " + string(v))
+		}
+		return nil
+	})
+	return m
+}
+
+func settingsToDB(m map[string]string) error {
+	fmt.Println(m)
+	db, err := bolt.Open("settings.db", 0644, nil)
+	if err != nil {
+		fmt.Printf("Error opening DB...%s\n", err.Error())
 	}
-	Settings = settingsFile
+	defer db.Close()
+	err = db.Update(func(tx *bolt.Tx) error {
+		fmt.Println("inside DB update function")
+		b, err := tx.CreateBucketIfNotExists([]byte("settings"))
+		if err != nil {
+			return fmt.Errorf("Bucket error: %s", err.Error())
+		}
+		for k, v := range m {
+			err := b.Put([]byte(k), []byte(v))
+			fmt.Println("Inserting --> " + k + ": " + v)
+			if err != nil {
+				return fmt.Errorf("Error inserting key/value pair into DB bucket...%s", err.Error())
+			}
+		}
+		fmt.Println("exiting DB update function")
+		return nil
+	})
+	fmt.Println("exiting settingsToDB function")
+	return err
 }
